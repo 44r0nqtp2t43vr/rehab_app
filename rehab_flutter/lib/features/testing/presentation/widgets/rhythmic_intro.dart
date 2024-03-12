83fe7a54c11415920e531d7a87c71c2de7439c15
unknown <aaronalcuitas16@gmail.com>
2024-03-12 10:27:49 +0800

    completed intro screens

--- rehab_flutter/lib/features/testing/presentation/widgets/rhythmic_intro.dart
+++ rehab_flutter/lib/features/testing/presentation/widgets/rhythmic_intro.dart
@@ -1,6 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_bloc.dart';
+import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_event.dart';
 import 'package:rehab_flutter/core/widgets/app_button.dart';
+import 'package:rehab_flutter/features/testing/data/data_sources/testing_data_provider.dart';
+import 'package:rehab_flutter/features/testing/domain/entities/rhythmic_pattern.dart';
 import 'package:rehab_flutter/features/testing/domain/enums/testing_enums.dart';
+import 'package:rehab_flutter/injection_container.dart';
 
 class RhythmicPatternsIntro extends StatefulWidget {
   final void Function(TestingState) onProceed;
@@ -12,13 +19,65 @@ class RhythmicPatternsIntro extends StatefulWidget {
 }
 
 class _RhythmicPatternsIntroState extends State<RhythmicPatternsIntro> {
+  final int patternDelay = 500;
+  final int durationPerRhythmicPattern = 10;
+  RhythmicPattern currentRhythmicPattern = TestingDataProvider.rhythmicPatterns[0];
+  int currentRhythmicPatternInd = 0;
+  Timer? _patternTimer;
+
+  void _onAnimationFinish() {
+    if (currentRhythmicPatternInd == TestingDataProvider.rhythmicPatterns.length - 1) {
+      stopPattern();
+    } else if (currentRhythmicPatternInd < TestingDataProvider.rhythmicPatterns.length - 1) {
+      stopPattern();
+      setState(() {
+        currentRhythmicPatternInd++;
+        currentRhythmicPattern = TestingDataProvider.rhythmicPatterns[currentRhythmicPatternInd];
+      });
+      startPattern();
+    }
+  }
+
+  void sendPattern(String data) {
+    sl<BluetoothBloc>().add(WriteDataEvent(data));
+  }
+
+  void startPattern() {
+    _patternTimer = Timer.periodic(Duration(milliseconds: patternDelay), (timer) {
+      sendPattern(currentRhythmicPattern.pattern[timer.tick % currentRhythmicPattern.pattern.length]);
+
+      // Check if the elapsed time exceeds the specified duration
+      if (timer.tick * patternDelay >= (durationPerRhythmicPattern * 1000)) {
+        _onAnimationFinish();
+      }
+    });
+  }
+
+  void stopPattern() {
+    _patternTimer?.cancel();
+    sendPattern("<000000000000000000000000000000>");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startPattern();
+  }
+
+  @override
+  void dispose() {
+    _patternTimer?.cancel();
+    sl<BluetoothBloc>().add(const WriteDataEvent("<000000000000000000000000000000>"));
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Expanded(
           flex: 2,
-          child: Container(),
+          child: Text(currentRhythmicPattern.name),
         ),
         Expanded(
           flex: 1,
--- rehab_flutter/lib/features/testing/presentation/widgets/textures_intro.dart
+++ rehab_flutter/lib/features/testing/presentation/widgets/textures_intro.dart
@@ -1,6 +1,19 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rehab_flutter/core/bloc/actuators/actuators_bloc.dart';
+import 'package:rehab_flutter/core/bloc/actuators/actuators_event.dart';
+import 'package:rehab_flutter/core/bloc/actuators/actuators_state.dart';
+import 'package:rehab_flutter/core/controller/actuators_controller.dart';
+import 'package:rehab_flutter/core/entities/actuators_imagedata.dart';
+import 'package:rehab_flutter/core/entities/actuators_initdata.dart';
+import 'package:rehab_flutter/core/enums/actuators_enums.dart';
 import 'package:rehab_flutter/core/widgets/app_button.dart';
+import 'package:rehab_flutter/features/scrolling_actuators/data/data_sources/anipattern_provider.dart';
+import 'package:rehab_flutter/features/testing/data/data_sources/testing_data_provider.dart';
 import 'package:rehab_flutter/features/testing/domain/enums/testing_enums.dart';
+import 'package:rehab_flutter/features/texture_therapy/domain/entities/image_texture.dart';
+import 'package:rehab_flutter/injection_container.dart';
 
 class TexturesIntro extends StatefulWidget {
   final void Function(TestingState) onProceed;
@@ -11,28 +24,136 @@ class TexturesIntro extends StatefulWidget {
   State<TexturesIntro> createState() => _TexturesIntroState();
 }
 
-class _TexturesIntroState extends State<TexturesIntro> {
+class _TexturesIntroState extends State<TexturesIntro> with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  ImageTexture currentImageTexture = TestingDataProvider.imageTextures[0];
+  int currentImageTextureInd = 0;
+  bool isPlaying = false;
+
+  void _onAnimationFinish() {
+    if (currentImageTextureInd < TestingDataProvider.imageTextures.length - 1) {
+      setState(() {
+        currentImageTextureInd++;
+        currentImageTexture = TestingDataProvider.imageTextures[currentImageTextureInd];
+        isPlaying = true;
+      });
+      sl<ActuatorsBloc>().add(LoadImageEvent(ActuatorsImageData(src: currentImageTexture.texture, preload: false)));
+      animationController.forward();
+    }
+  }
+
+  void _renderActuators(double imageSize) {
+    final Offset animatedPosition = AniPatternProvider().verticalPattern(imageSize, animationController.value);
+    sl<ActuatorsBloc>().add(UpdateActuatorsEvent(animatedPosition));
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    );
+
+    animationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isPlaying = false;
+        });
+        animationController.reset();
+        _onAnimationFinish();
+      }
+    });
+    animationController.addListener(() {
+      setState(() {});
+    });
+
+    isPlaying = true;
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 2,
-          child: Container(),
-        ),
-        Expanded(
-          flex: 1,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AppButton(
-                onPressed: () => widget.onProceed(TestingState.textures),
-                child: const Text('Proceed'),
-              ),
-            ],
+    int desiredSize = MediaQuery.of(context).size.width.toInt();
+
+    if (isPlaying) {
+      _renderActuators(desiredSize.toDouble());
+    }
+
+    return BlocProvider(
+      create: (_) => sl<ActuatorsBloc>()
+        ..add(InitActuatorsEvent(ActuatorsInitData(
+          imgSrc: currentImageTexture.texture,
+          orientation: ActuatorsOrientation.landscape,
+          numOfFingers: ActuatorsNumOfFingers.five,
+          imagesHeight: desiredSize,
+          imagesWidth: desiredSize,
+        ))),
+      child: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: _buildBody(currentImageTexture, desiredSize),
           ),
-        ),
-      ],
+          Expanded(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                AppButton(
+                  onPressed: () => widget.onProceed(TestingState.textures),
+                  child: const Text('Proceed'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBody(ImageTexture currentTexture, int desiredSize) {
+    return BlocBuilder<ActuatorsBloc, ActuatorsState>(
+      builder: (context, state) {
+        if (state is ActuatorsLoading) {
+          return const Center(child: CupertinoActivityIndicator());
+        } else if (state is ActuatorsDone) {
+          return Center(
+            child: Column(
+              children: <Widget>[
+                const Spacer(),
+                Stack(
+                  children: [
+                    Container(
+                      width: desiredSize.toDouble(),
+                      height: desiredSize.toDouble(),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(currentTexture.image),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    ...sl<ActuatorsController>().buildActuators(),
+                  ],
+                ),
+                Text(currentImageTexture.name),
+                const Spacer(),
+              ],
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
