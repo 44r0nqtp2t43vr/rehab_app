f221b4f09fedbe7c2fc5ae9c5961c529b6101914
unknown <aaronalcuitas16@gmail.com>
2024-03-26 14:20:04 +0800

    completed daily progress functionality

--- rehab_flutter/lib/features/tab_home/presentation/widgets/daily_progress_card.dart
+++ rehab_flutter/lib/features/tab_home/presentation/widgets/daily_progress_card.dart
@@ -6,6 +6,15 @@ class DailyProgressCard extends StatelessWidget {
 
   const DailyProgressCard({super.key, required this.todaySession});
 
+  double getPercentFromTodaySession() {
+    if (todaySession == null) {
+      return 0;
+    }
+
+    final List<bool> conditions = todaySession!.getSessionConditions();
+    return conditions.where((condition) => condition == true).length * (100 / conditions.length);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,6 +30,16 @@ class DailyProgressCard extends StatelessWidget {
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10.0),
               border: Border.all(color: Colors.white),
+            ),
+            child: Center(
+              child: Text(
+                "${getPercentFromTodaySession().toStringAsFixed(0)}%",
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
             ),
           ),
           const Spacer(),
@@ -50,62 +69,170 @@ class DailyProgressCard extends StatelessWidget {
   }
 
   void _onSeeDailyProgress(Session? session, BuildContext context) {
-    final List<bool> conditions = session!.getSessionConditions();
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) {
-        return AlertDialog(
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const Row(
-                children: [
-                  Icon(
-                    Icons.arrow_upward,
-                    size: 40,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Daily",
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
+    if (session == null) {
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.arrow_upward,
+                      size: 40,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Daily",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "Progress",
-                        style: TextStyle(
-                          fontSize: 12,
+                        Text(
+                          "Progress",
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 28),
+                const Text(
+                  "You have no sessions for today",
+                  style: TextStyle(
+                    fontSize: 12,
                   ),
-                ],
-              ),
-              const SizedBox(height: 28),
-              Row(
-                children: [
-                  Radio(
-                    value: conditions[0],
-                    groupValue: true,
-                    onChanged: (value) {},
-                  ),
-                  const Text("Completed Pre-test"),
-                ],
-              ),
-              const SizedBox(height: 28),
-              ElevatedButton(
-                onPressed: () => _onCloseButtonPressed(context),
-                child: const Text("CLOSE"),
-              ),
-            ],
-          ),
-        );
-      },
-    );
+                ),
+                const SizedBox(height: 28),
+                ElevatedButton(
+                  onPressed: () => _onCloseButtonPressed(context),
+                  child: const Text("CLOSE"),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    } else {
+      final List<bool> conditions = session.getSessionConditions();
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                const Row(
+                  children: [
+                    Icon(
+                      Icons.arrow_upward,
+                      size: 40,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Daily",
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Progress",
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 28),
+                Row(
+                  children: [
+                    Radio(
+                      value: conditions[0],
+                      groupValue: true,
+                      onChanged: (value) {},
+                    ),
+                    const Expanded(
+                      child: Text("Completed Pre-test"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Radio(
+                      value: conditions[1],
+                      groupValue: true,
+                      onChanged: (value) {},
+                    ),
+                    const Expanded(
+                      child: Text("Completed 1st Standard Session"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Radio(
+                      value: conditions[2],
+                      groupValue: true,
+                      onChanged: (value) {},
+                    ),
+                    const Expanded(
+                      child: Text("Completed Passive Session"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Radio(
+                      value: conditions[3],
+                      groupValue: true,
+                      onChanged: (value) {},
+                    ),
+                    const Expanded(
+                      child: Text("Completed 2nd Standard Session"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Radio(
+                      value: conditions[4],
+                      groupValue: true,
+                      onChanged: (value) {},
+                    ),
+                    const Expanded(
+                      child: Text("Completed Post-test"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 28),
+                ElevatedButton(
+                  onPressed: () => _onCloseButtonPressed(context),
+                  child: const Text("CLOSE"),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
   }
 
   void _onCloseButtonPressed(BuildContext context) {
