3ca47c457169e19fa86a5e8d964e6467f8641ba9
unknown <aaronalcuitas16@gmail.com>
2024-03-28 17:07:49 +0800

    added timer to standard therapy

--- rehab_flutter/lib/features/standard_therapy/presentation/pages/standard_therapy_screen/standard_therapy_screen.dart
+++ rehab_flutter/lib/features/standard_therapy/presentation/pages/standard_therapy_screen/standard_therapy_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:rehab_flutter/features/standard_therapy/domain/entities/standard_therapy_data.dart';
 
@@ -11,15 +13,57 @@ class StandardTherapyScreen extends StatefulWidget {
 }
 
 class _StandardTherapyScreenState extends State<StandardTherapyScreen> {
+  late Timer timer;
+  int countdownDuration = 90;
+
+  @override
+  void initState() {
+    startCountdown();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+
+  void startCountdown() {
+    const oneSec = Duration(seconds: 1);
+    timer = Timer.periodic(
+      oneSec,
+      (Timer timer) {
+        setState(() {
+          if (countdownDuration < 1) {
+            timer.cancel();
+          } else {
+            countdownDuration -= 1;
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          "${widget.data.type.name} ${widget.data.intensity}",
-          style: const TextStyle(
-            color: Colors.white,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "$countdownDuration",
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "${widget.data.type.name} ${widget.data.intensity}",
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ],
         ),
       ),
     );
