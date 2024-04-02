6048d66acf45fa7c40eabd3ae38832698f4c56d7
unknown <aaronalcuitas16@gmail.com>
2024-04-02 15:25:13 +0800

    implemented standard therapy musicVisualizer

--- rehab_flutter/lib/features/actuator_therapy/presentation/pages/actuator_therapy_screen.dart
+++ rehab_flutter/lib/features/actuator_therapy/presentation/pages/actuator_therapy_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';
 import 'package:rehab_flutter/config/theme/app_themes.dart';
 import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_bloc.dart';
 import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_event.dart';
--- rehab_flutter/lib/features/standard_therapy/presentation/widgets/st_visualizer.dart
+++ rehab_flutter/lib/features/standard_therapy/presentation/widgets/st_visualizer.dart
@@ -1,6 +1,23 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:math';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rehab_flutter/core/bloc/firebase/user/user_bloc.dart';
+import 'package:rehab_flutter/core/bloc/firebase/user/user_event.dart';
 import 'package:rehab_flutter/core/entities/song.dart';
 import 'package:rehab_flutter/core/entities/user.dart';
+import 'package:rehab_flutter/features/piano_tiles/presentation/widgets/song_slider.dart';
+import 'package:rehab_flutter/features/standard_therapy/domain/entities/standard_data.dart';
+import 'package:rehab_flutter/features/visualizer_therapy_slider/domain/controllers/bluetooth_controller.dart';
+import 'package:rehab_flutter/features/visualizer_therapy_slider/domain/controllers/helper_functions.dart';
+import 'package:rehab_flutter/features/visualizer_therapy_slider/domain/models/audio_data.dart';
+import 'package:rehab_flutter/features/visualizer_therapy_slider/domain/models/ray_painter_state.dart';
+import 'package:rehab_flutter/features/visualizer_therapy_slider/presentation/widgets/circle_painter.dart';
 
 class STVisualizer extends StatefulWidget {
   final AppUser user;
@@ -16,13 +33,443 @@ class STVisualizer extends StatefulWidget {
   State<STVisualizer> createState() => _STVisualizerState();
 }
 
-class _STVisualizerState extends State<STVisualizer> {
+class _STVisualizerState extends State<STVisualizer> with SingleTickerProviderStateMixin {
+  //  controllers
+  late AudioPlayer audioPlayer;
+  late AnimationController _controller;
+  late StreamSubscription? positionSubscription;
+
+  // initial values
+  final double _totalHeight = 100.0;
+  final double _totalWidth = 100.0;
+  final double _circleHeight = 20.0;
+  final double _circleWidth = 20.0;
+  final double _rayHeight = -8.0;
+  final double _rayWidth = 60.0;
+  final Color _color = Colors.blue;
+  bool isPlaying = false;
+
+// list of circles
+  List<dynamic> circles = [];
+
+  // AudioData variables
+  List<dynamic> blocks = [];
+  int currentIndex = -1;
+  int prevIndex = -1;
+
+  // Cols
+  List<int> firstCol = [0, 4, 8, 12];
+  List<int> secondCol = [1, 5, 9, 13];
+  List<int> thirdCol = [2, 6, 10, 14];
+  List<int> fourthCol = [3, 7, 11, 15];
+
+// Rows
+  List<int> firstRow = [0, 1, 2, 3];
+  List<int> secondRow = [4, 5, 6, 7];
+  List<int> thirdRow = [8, 9, 10, 11];
+  List<int> fourthRow = [12, 13, 14, 15];
+
+  // squares
+  List<int> firstSquare = [0, 1, 4, 5];
+  List<int> secondSquare = [2, 3, 6, 7];
+  List<int> thirdSquare = [8, 9, 12, 13];
+  List<int> fourthSquare = [10, 11, 14, 15];
+  List<int> innerSquare = [5, 6, 9, 10];
+  List<int> outerSquare = [0, 1, 2, 3, 4, 7, 8, 11, 12, 13, 14, 15];
+
+  // frequencies
+  List<int> bassSquare = [2, 3, 6, 7];
+  List<int> midRangeSquare = [9, 10];
+  List<int> subBassSquare = [1, 4];
+  List<int> lowerMidrangeSquare = [8, 12];
+  List<int> presenceSquare = [13, 14];
+  List<int> higherMidrangeSquare = [11, 15];
+  List<int> brillianceSquare = [0, 5];
+
+  // actuator vars
+  int leftActuatorSum = 0;
+  int rightActuatorSum = 0;
+  List<int> activeValues = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  Map<String, int> lastSentPattern = {};
+  double currentPositionSec = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 850),
+    )..addListener(() {
+        setState(() {});
+      });
+    audioPlayer = AudioPlayer();
+    isPlaying = true;
+    // Start playing the audio
+
+    audioPlayer.setSource(AssetSource(widget.song.audioSource)).then((_) {
+      audioPlayer.seek(const Duration(seconds: 0));
+      audioPlayer.resume();
+    });
+
+    audioPlayer.play(AssetSource(widget.song.audioSource), position: const Duration(seconds: 0));
+
+    _controller.repeat(reverse: true);
+    circles = List.generate(16, (index) {
+      // Assuming you want 16 blocks as per your UI setup
+      return RayPainterState(
+        id: '$index',
+        progress: _controller.value,
+        totalHeight: _totalHeight,
+        totalWidth: _totalWidth,
+        circleHeight: _circleHeight,
+        circleWidth: _circleWidth,
+        rayHeight: _rayHeight,
+        rayWidth: _rayWidth,
+        color: _color,
+      );
+    });
+    _controller.addListener(() {
+      setState(() {
+        // Update the progress value for each circle
+        for (var circle in circles) {
+          circle.progress = _controller.value;
+        }
+      });
+    });
+
+    // Load blocks from forest_of_blocks.json
+    loadBlocks(widget.song.metaDataUrl);
+    // Subscribe to the onPositionChanged event
+
+    positionSubscription = audioPlayer.onPositionChanged.listen((position) {
+      setState(() {
+        currentPositionSec = position.inSeconds.toDouble();
+      });
+
+      useClosestBlock(position.inMilliseconds / 1000.0); // Convert milliseconds to seconds
+    });
+
+    audioPlayer.onPlayerComplete.listen((_) {
+      BlocProvider.of<UserBloc>(context).add(SubmitStandardEvent(StandardData(userId: widget.user.userId, isStandardOne: true)));
+    });
+  }
+
+  void _pauseAnimation() {
+    audioPlayer.pause();
+    setState(() {
+      isPlaying = false;
+    });
+  }
+
+  void _resumeAnimation() {
+    audioPlayer.resume();
+    setState(() {
+      isPlaying = true;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    positionSubscription?.cancel();
+    // Cancel the subscription to avoid memory leaks
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text(widget.song.title),
+        const Spacer(),
+        Column(
+          children: [
+            ...buildRayPainterRows(),
+          ],
+        ),
+        Expanded(
+          flex: 3,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.song.title,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Sailec Medium',
+                    fontSize: 20,
+                    height: 1.2,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  widget.song.artist,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Sailec Light',
+                    fontSize: 16,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(height: 8),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Row(
+                    children: [
+                      Text(
+                        secToMinSec(currentPositionSec),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Sailec Light',
+                          fontSize: 12,
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Expanded(
+                        child: SongSlider(
+                          currentDuration: currentPositionSec,
+                          minDuration: 0,
+                          maxDuration: widget.song.duration,
+                          onDurationChanged: (value) {},
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      Text(
+                        widget.song.songTime,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'Sailec Light',
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        icon: const Icon(
+                          CupertinoIcons.shuffle,
+                          size: 24,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          CupertinoIcons.backward_end_fill,
+                          size: 24,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          isPlaying ? CupertinoIcons.pause_fill : CupertinoIcons.play_arrow_solid,
+                          size: 40,
+                          color: Colors.white,
+                        ),
+                        onPressed: () => isPlaying ? _pauseAnimation() : _resumeAnimation(),
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          CupertinoIcons.forward_end_fill,
+                          size: 24,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: const Icon(
+                          CupertinoIcons.square_list_fill,
+                          size: 24,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ],
     );
+  }
+
+  String secToMinSec(double seconds) {
+    int roundedSeconds = seconds.round();
+    int minutes = roundedSeconds ~/ 60;
+    int remainingSeconds = roundedSeconds % 60;
+    return "${minutes.toString().padLeft(2, '0')}:${remainingSeconds.toString().padLeft(2, '0')}";
+  }
+
+  List<Widget> buildRayPainterRows() {
+    List<Widget> rows = [];
+    int itemsPerRow = 4; // Number of RayPainters per row
+    for (int i = 0; i < circles.length; i += itemsPerRow) {
+      List<Widget> rowItems = circles.sublist(i, min(i + itemsPerRow, circles.length)).map((circleState) {
+        return SizedBox(
+          width: 100,
+          height: 100,
+          child: CustomPaint(
+            painter: RayPainter(
+              progress: circleState.progress,
+              totalHeight: circleState.totalHeight,
+              totalWidth: circleState.totalWidth,
+              circleHeight: circleState.circleHeight,
+              circleWidth: circleState.circleWidth,
+              rayHeight: circleState.rayHeight,
+              rayWidth: circleState.rayWidth,
+              color: circleState.color,
+            ),
+          ),
+        );
+      }).toList();
+      rows.add(Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: rowItems));
+    }
+    return rows;
+  }
+
+  void updateCircleState(int index, Color color, double width, double height, int activeValue) {
+    setState(() {
+      circles[index].color = color;
+      circles[index].circleWidth = width;
+      circles[index].circleHeight = height;
+
+      activeValues[index] = activeValue;
+    });
+    lastSentPattern = sendUpdatedPattern(activeValues, lastSentPattern);
+  }
+
+  void resetAllCircles(Color color, double width, double height, int activeValue) {
+    setState(() {
+      for (var circle in circles) {
+        circle.color = color;
+        circle.circleWidth = width;
+        circle.circleHeight = height;
+      }
+      for (int i = 0; i < activeValues.length; i++) {
+        activeValues[i] = activeValue;
+      }
+    });
+  }
+
+  void updateCirclePropertiesNoteOnset(List<int> squares, bool isActive) {
+    final double size = isActive ? 40.0 : 20.0;
+    final int activeValue = isActive ? 1 : 0;
+
+    for (int i = 0; i < squares.length; i++) {
+      circles[squares[i]].circleWidth = size;
+      circles[squares[i]].circleHeight = size;
+      circles[squares[i]].color = isActive ? Colors.green : Colors.blue;
+      activeValues[squares[i]] = activeValue;
+    }
+  }
+
+  void updateCircleProperties(List<int> squares, bool isActive) {
+    final double size = isActive ? 40.0 : 20.0;
+    final int activeValue = isActive ? 1 : 0;
+
+    for (int i = 0; i < squares.length; i++) {
+      circles[squares[i]].circleWidth = size;
+      circles[squares[i]].circleHeight = size;
+      circles[squares[i]].color = isActive ? Colors.yellow : Colors.blue;
+      activeValues[squares[i]] = activeValue;
+    }
+  }
+
+  void loadBlocks(String audioData) async {
+    String data = await rootBundle.loadString(audioData);
+    final List<dynamic> blockJson = json.decode(data);
+    blocks = blockJson.map((json) => AudioData.fromJson(json)).toList();
+  }
+
+  void useClosestBlock(double positionSec) {
+    if (blocks.isEmpty) return;
+
+    for (int i = 0; i < blocks.length; i++) {
+      if (i == blocks.length - 1 || (blocks[i].time <= positionSec && blocks[i + 1].time > positionSec)) {
+        if (i != currentIndex) {
+          setState(() {
+            prevIndex = currentIndex; // Save the current index as previous before updating
+            currentIndex = i; // Now update the current index
+          });
+          break;
+        }
+      }
+    }
+    var midRange = blocks[currentIndex].midrange;
+    var higherMidrange = blocks[currentIndex].higherMidrange;
+    var bass = blocks[currentIndex].bass;
+    var subBass = blocks[currentIndex].subBass;
+    var lowerMidrange = blocks[currentIndex].lowerMidrange;
+    var presence = blocks[currentIndex].presence;
+    var noteOnset = blocks[currentIndex].noteOnset;
+    var brilliance = blocks[currentIndex].brilliance;
+
+    setState(() {
+      // 0 for item in ActiveValues
+      for (int i = 0; i < activeValues.length; i++) {
+        activeValues[i] = 0;
+      }
+
+      if (noteOnset == 1 && blocks[prevIndex].noteOnset == 0 && blocks[prevIndex - 1].noteOnset == 0 && blocks[prevIndex - 2].noteOnset == 0 && blocks[prevIndex - 3].noteOnset == 0) {
+        for (int i = 0; i < activeValues.length; i++) {
+          activeValues[i] = 0;
+        }
+        int delay = 0; // Initial delay is 0ms for the first column
+        List<List<int>> columns = [firstCol, secondCol, thirdCol, fourthCol]; // List of column groups for iteration
+
+        for (var column in columns) {
+          Timer(Duration(milliseconds: delay), () {
+            for (var index in column) {
+              // Reset active values before setting the current column as active
+              for (int i = 0; i < activeValues.length; i++) {
+                activeValues[i] = 0;
+              }
+              updateCircleState(index, Colors.green, 40.0, 40.0, 1); // Mark as active
+            }
+          });
+
+          // Increase delay by 200ms for the next column
+          delay += 200;
+        }
+// Schedule the reset to blue for all circles, to happen after the last column has turned green
+        Timer(Duration(milliseconds: delay), () {
+          resetAllCircles(Colors.blue, 20.0, 20.0, 0); // Mark all as inactive
+        });
+      } else if (noteOnset == 1) {
+        updateCirclePropertiesNoteOnset(outerSquare, true);
+        lastSentPattern = sendUpdatedPattern(activeValues, lastSentPattern);
+      } else if (activeValues.every((value) => value == 0)) //active values is all 0
+      {
+        updateCircleProperties(bassSquare, getBassBoolValue(bass));
+        updateCircleProperties(midRangeSquare, getMidrangeBoolValue(midRange));
+        updateCircleProperties(lowerMidrangeSquare, getLowerMidrangeBoolValue(lowerMidrange));
+        updateCircleProperties(subBassSquare, getSubBassBoolValue(subBass));
+        updateCircleProperties(presenceSquare, getPresenceBoolValue(presence));
+        updateCircleProperties(higherMidrangeSquare, getUpperMidrangeBoolValue(higherMidrange));
+        updateCircleProperties(brillianceSquare, getBrillianceBoolValue(brilliance));
+      }
+
+      lastSentPattern = sendUpdatedPattern(activeValues, lastSentPattern);
+    });
+
+    // Ensure a rebuild to reflect color and size changes
   }
 }
