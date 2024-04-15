7ee124107a083da0c427211f39183c0bd3c446bc
unknown <aaronalcuitas16@gmail.com>
2024-04-15 16:23:11 +0800

    setup physician dashboard

---
+++ rehab_flutter/lib/features/patients_manager/presentation/widgets/physician_numbers.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PhysicianNumbers extends StatelessWidget {
+  const PhysicianNumbers({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [],
+    );
+  }
+}
