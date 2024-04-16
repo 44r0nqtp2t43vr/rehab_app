6b7467d209656e7688a5efdb1386db65355d53c7
unknown <aaronalcuitas16@gmail.com>
2024-04-16 16:06:43 +0800

    fixed merge errors

--- rehab_flutter/lib/features/patients_manager/presentation/pages/physician_profile/physician_profile.dart
+++ rehab_flutter/lib/features/patients_manager/presentation/pages/physician_profile/physician_profile.dart
@@ -1,7 +1,7 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
 import 'package:rehab_flutter/config/theme/app_themes.dart';
 import 'package:rehab_flutter/core/bloc/firebase/physician/physician_bloc.dart';
@@ -18,8 +18,6 @@ class PhysicianProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       physics: const BouncingScrollPhysics(),
-
-      child: SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
         child: Column(
@@ -50,23 +48,12 @@ class PhysicianProfile extends StatelessWidget {
                   radius: 34,
                   child: physician.imageURL != null
                       ? ClipOval(
-                          child: CachedNetworkImage(
-                            imageUrl: physician.imageURL!,
-                            fit: BoxFit.cover,
-                            width: double.infinity,
-                            height: double.infinity,
-                            placeholder: (context, url) =>
-                                const CircularProgressIndicator(),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
-                          ),
-                          //   Image.network(
-                          //   physician.imageURL!,
-                          //   fit: BoxFit.cover,
-                          //   width: double.infinity,
-                          //   height: double.infinity,
-                          // ),
-                        )
+                          child: Image.network(
+                          physician.imageURL!,
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                          height: double.infinity,
+                        ))
                       : const Center(
                           child: Padding(
                             padding: EdgeInsets.all(4),
@@ -84,7 +71,7 @@ class PhysicianProfile extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        physician.getUserFullName(),
+                        "${physician.firstName.capitalize!} ${physician.lastName.capitalize!}",
                         style: const TextStyle(
                           fontFamily: 'Sailec Bold',
                           fontSize: 18,
@@ -135,93 +122,61 @@ class PhysicianProfile extends StatelessWidget {
               child: Padding(
                 padding: const EdgeInsets.all(24),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text(
-                      "Profile",
-                      style: darkTextTheme().headlineLarge,
+                    ProfileButton(
+                      onTap: () => _onAssignPatientsButtonPressed(context),
+                      icon: CupertinoIcons.person_fill,
+                      text: "Assign Patients",
                     ),
-                    Text(
-                      "Account Settings",
-                      style: darkTextTheme().headlineSmall,
+                    const SizedBox(height: 20),
+                    ProfileButton(
+                      onTap: () {},
+                      icon: Icons.assignment,
+                      text: "Terms of Services",
+                    ),
+                    const SizedBox(height: 20),
+                    ProfileButton(
+                      onTap: () {},
+                      icon: Icons.security,
+                      text: "Privacy Policy",
                     ),
                   ],
                 ),
               ),
-              const SizedBox(height: 28),
-              Row(
-                children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                    radius: 34,
-                    child: physician.imageURL != null
-                        ? ClipOval(
-                            child: CachedNetworkImage(
-                              imageUrl: physician.imageURL!,
-                              fit: BoxFit.cover,
-                              width: double.infinity,
-                              height: double.infinity,
-                              placeholder: (context, url) =>
-                                  const CircularProgressIndicator(),
-                              errorWidget: (context, url, error) =>
-                                  const Icon(Icons.error),
-                            ),
-                            //   Image.network(
-                            //   physician.imageURL!,
-                            //   fit: BoxFit.cover,
-                            //   width: double.infinity,
-                            //   height: double.infinity,
-                            // ),
-                          )
-                        : const Center(
-                            child: Padding(
-                              padding: EdgeInsets.all(4),
-                              child: Icon(
-                                CupertinoIcons.profile_circled,
-                                color: Colors.white,
-                                size: 60,
-                              ),
-                            ),
-                          ),
-                  ),
-                  const SizedBox(width: 20),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "${physician.firstName.capitalize!} ${physician.lastName.capitalize!}",
-                          style: const TextStyle(
-                            fontFamily: 'Sailec Bold',
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          physician.email,
-                          style: darkTextTheme().headlineSmall,
+            ),
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: const LinearGradient(
+                        colors: [
+                          Color(0xFF128BED),
+                          Color(0xFF01FF99),
+                        ],
+                        stops: [0.3, 1.0],
+                        begin: Alignment.centerLeft,
+                        end: Alignment.centerRight,
+                      ),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.05),
+                          spreadRadius: 10,
+                          blurRadius: 20,
+                          offset: const Offset(1, 1),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
                     child: ElevatedButton(
-                      onPressed: () => _onEditProfileButtonPressed(context),
+                      onPressed: () => _onLogoutButtonPressed(context),
                       style: ButtonStyle(
                         foregroundColor: MaterialStateProperty.all<Color>(
                           Colors.white,
                         ),
-
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            const Color(0xff128BED)),
-
+                        backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
                         elevation: MaterialStateProperty.all<double>(0),
                         shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
                         overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
@@ -231,96 +186,13 @@ class PhysicianProfile extends StatelessWidget {
                           ),
                         ),
                       ),
-                      child: const Text("Edit Profile"),
+                      child: const Text("Logout"),
                     ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 20),
-              GlassContainer(
-                shadowStrength: 2,
-                shadowColor: Colors.black,
-                blur: 4,
-                color: Colors.white.withOpacity(0.25),
-                child: Padding(
-                  padding: const EdgeInsets.all(24),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      ProfileButton(
-                        onTap: () => _onAssignPatientsButtonPressed(context),
-                        icon: CupertinoIcons.person_fill,
-                        text: "Assign Patients",
-                      ),
-                      const SizedBox(height: 20),
-                      ProfileButton(
-                        onTap: () {},
-                        icon: Icons.assignment,
-                        text: "Terms of Services",
-                      ),
-                      const SizedBox(height: 20),
-                      ProfileButton(
-                        onTap: () {},
-                        icon: Icons.security,
-                        text: "Privacy Policy",
-                      ),
-                    ],
                   ),
                 ),
-              ),
-              const SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                          colors: [
-                            Color(0xFF128BED),
-                            Color(0xFF01FF99),
-                          ],
-                          stops: [0.3, 1.0],
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                        ),
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.05),
-                            spreadRadius: 10,
-                            blurRadius: 20,
-                            offset: const Offset(1, 1),
-                          ),
-                        ],
-                      ),
-                      child: ElevatedButton(
-                        onPressed: () => _onLogoutButtonPressed(context),
-                        style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all<Color>(
-                            Colors.white,
-                          ),
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Colors.transparent),
-                          elevation: MaterialStateProperty.all<double>(0),
-                          shadowColor: MaterialStateProperty.all<Color>(
-                              Colors.transparent),
-                          overlayColor: MaterialStateProperty.all<Color>(
-                              Colors.transparent),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                        ),
-                        child: const Text("Logout"),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+              ],
+            ),
+          ],
         ),
       ),
     );
