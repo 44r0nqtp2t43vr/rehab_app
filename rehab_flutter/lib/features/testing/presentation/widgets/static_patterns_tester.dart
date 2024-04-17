b8f95fce77a9c687dfed1911f4af8f215d4264f1
unknown <aaronalcuitas16@gmail.com>
2024-04-17 16:50:29 +0800

    implemented revision on static patterns test

--- rehab_flutter/lib/features/testing/presentation/widgets/static_patterns_tester.dart
+++ rehab_flutter/lib/features/testing/presentation/widgets/static_patterns_tester.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:rehab_flutter/config/theme/app_themes.dart';
 import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_bloc.dart';
@@ -29,38 +31,19 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
   final List<GlobalKey> _circleKeys = List.generate(16, (index) => GlobalKey());
   final List<bool> _circleStates = List.generate(16, (_) => false);
   final List<bool> _isCircleStateUpdated = List.generate(16, (_) => false);
+  Timer? _timer;
 
   void _sendPattern() {
     String currentPatternString = widget.currentStaticPattern.pattern;
-    String data =
-        "<$currentPatternString$currentPatternString$currentPatternString$currentPatternString$currentPatternString>";
+    String data = "<$currentPatternString$currentPatternString$currentPatternString$currentPatternString$currentPatternString>";
 
     sl<BluetoothBloc>().add(WriteDataEvent(data));
     debugPrint("Pattern sent: $data");
   }
 
   int _sideAndValueToCircleStateIndex(bool isLeft, int value) {
-    final List<int> cursorValues = [
-      1,
-      8,
-      1,
-      8,
-      2,
-      16,
-      2,
-      16,
-      4,
-      32,
-      4,
-      32,
-      64,
-      128,
-      64,
-      128
-    ];
-    return isLeft
-        ? cursorValues.indexOf(value)
-        : cursorValues.lastIndexOf(value);
+    final List<int> cursorValues = [1, 8, 1, 8, 2, 16, 2, 16, 4, 32, 4, 32, 64, 128, 64, 128];
+    return isLeft ? cursorValues.indexOf(value) : cursorValues.lastIndexOf(value);
   }
 
   List<bool> _patternToCircleStates(String pattern) {
@@ -75,13 +58,11 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
     for (int i = 0; i < actuatorValues.length; i++) {
       if (left - actuatorValues[i] >= 0) {
         left -= actuatorValues[i];
-        circleStates[_sideAndValueToCircleStateIndex(true, actuatorValues[i])] =
-            true;
+        circleStates[_sideAndValueToCircleStateIndex(true, actuatorValues[i])] = true;
       }
       if (right - actuatorValues[i] >= 0) {
         right -= actuatorValues[i];
-        circleStates[
-            _sideAndValueToCircleStateIndex(false, actuatorValues[i])] = true;
+        circleStates[_sideAndValueToCircleStateIndex(false, actuatorValues[i])] = true;
       }
     }
 
@@ -90,8 +71,7 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
 
   double _calculateAccuracy() {
     final List<bool> answers = _circleStates;
-    final List<bool> correctAnswers =
-        _patternToCircleStates(widget.currentStaticPattern.pattern);
+    final List<bool> correctAnswers = _patternToCircleStates(widget.currentStaticPattern.pattern);
     int correctlyAnsweredCount = 0;
 
     for (int i = 0; i < answers.length; i++) {
@@ -105,16 +85,12 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
 
   void _updateCircleStateBasedOnPosition(Offset globalPosition) {
     for (int i = 0; i < _circleKeys.length; i++) {
-      final RenderBox? box =
-          _circleKeys[i].currentContext?.findRenderObject() as RenderBox?;
+      final RenderBox? box = _circleKeys[i].currentContext?.findRenderObject() as RenderBox?;
       if (box != null) {
         final position = box.localToGlobal(Offset.zero);
         final size = box.size;
 
-        if (globalPosition.dx >= position.dx &&
-            globalPosition.dx <= position.dx + size.width &&
-            globalPosition.dy >= position.dy &&
-            globalPosition.dy <= position.dy + size.height) {
+        if (globalPosition.dx >= position.dx && globalPosition.dx <= position.dx + size.width && globalPosition.dy >= position.dy && globalPosition.dy <= position.dy + size.height) {
           setState(() {
             if (!_isCircleStateUpdated[i]) {
               _circleStates[i] = !_circleStates[i];
@@ -137,24 +113,34 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
     });
   }
 
+  void _sendPatternRepeatedly() {
+    // Cancel the previous timer if it exists
+    _timer?.cancel();
+
+    // Schedule a new timer to call _sendPattern every 3 seconds
+    _timer = Timer.periodic(const Duration(seconds: 3), (Timer timer) {
+      _sendPattern();
+    });
+  }
+
   @override
   void initState() {
     super.initState();
-    _sendPattern();
+    _sendPatternRepeatedly();
   }
 
   @override
   void didUpdateWidget(covariant StaticPatternsTester oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (widget.currentStaticPattern != oldWidget.currentStaticPattern) {
-      _sendPattern();
+      _sendPatternRepeatedly();
     }
   }
 
   @override
   void dispose() {
-    sl<BluetoothBloc>()
-        .add(const WriteDataEvent("<000000000000000000000000000000>"));
+    _timer?.cancel();
+    sl<BluetoothBloc>().add(const WriteDataEvent("<000000000000000000000000000000>"));
     super.dispose();
   }
 
@@ -163,8 +149,7 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
     return Column(
       children: [
         const SizedBox(height: 32),
-        TestLabel(
-            label: "Item ${widget.currentItemNo} of ${widget.totalItemNo}"),
+        TestLabel(label: "Item ${widget.currentItemNo} of ${widget.totalItemNo}"),
         const SizedBox(height: 16),
         Expanded(
           flex: 2,
@@ -182,8 +167,7 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
                     }
                   });
                 },
-                onPanUpdate: (DragUpdateDetails details) =>
-                    _updateCircleStateBasedOnPosition(details.globalPosition),
+                onPanUpdate: (DragUpdateDetails details) => _updateCircleStateBasedOnPosition(details.globalPosition),
                 onPanEnd: (DragEndDetails details) {
                   // Reset the flag when the touch ends
                   setState(() {
@@ -224,10 +208,8 @@ class _StaticPatternsTesterState extends State<StaticPatternsTester> {
                     const Color(0xff128BED),
                   ),
                   elevation: MaterialStateProperty.all<double>(0),
-                  shadowColor:
-                      MaterialStateProperty.all<Color>(Colors.transparent),
-                  overlayColor:
-                      MaterialStateProperty.all<Color>(Colors.transparent),
+                  shadowColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                  overlayColor: MaterialStateProperty.all<Color>(Colors.transparent),
                   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                     RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
