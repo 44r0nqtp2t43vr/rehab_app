ad68dc11a1c0408635f56e6d4b9367c27cba83ad
44r0nqtp2t43vr <aaronalcuitas16@gmail.com>
2025-01-22 17:27:20 +0800

    implemented tdt functionality

--- rehab_flutter/lib/features/testing/presentation/widgets/tdt_pairs_tester.dart
+++ rehab_flutter/lib/features/testing/presentation/widgets/tdt_pairs_tester.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rehab_flutter/core/bloc/actuators/actuators_bloc.dart';
+import 'package:rehab_flutter/core/bloc/actuators/actuators_event.dart';
+import 'package:rehab_flutter/core/bloc/actuators/actuators_state.dart';
 import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_bloc.dart';
 import 'package:rehab_flutter/core/bloc/bluetooth/bluetooth_event.dart';
+import 'package:rehab_flutter/core/controller/actuators_controller.dart';
+import 'package:rehab_flutter/core/data_sources/anipattern_provider.dart';
+import 'package:rehab_flutter/core/entities/actuators_initdata.dart';
 import 'package:rehab_flutter/core/entities/image_texture.dart';
+import 'package:rehab_flutter/core/enums/actuators_enums.dart';
+import 'package:rehab_flutter/core/interface/actuators_repository.dart';
 import 'package:rehab_flutter/features/testing/domain/entities/tdt_pair.dart';
 import 'package:rehab_flutter/features/testing/presentation/widgets/test_label.dart';
 import 'package:rehab_flutter/injection_container.dart';
@@ -26,8 +36,11 @@ class TdtPairsTester extends StatefulWidget {
   State<TdtPairsTester> createState() => _TdtPairsTesterState();
 }
 
-class _TdtPairsTesterState extends State<TdtPairsTester> {
+class _TdtPairsTesterState extends State<TdtPairsTester> with SingleTickerProviderStateMixin {
   // Timer? _timer;
+  late AnimationController animationController;
+  ImageTexture? _selectedTexture;
+  bool isPlaying = false;
 
   // double _calculateAccuracy(String answer) {
   //   return answer == widget.currentStaticPattern.name[0] ? 100 : 0;
@@ -35,6 +48,11 @@ class _TdtPairsTesterState extends State<TdtPairsTester> {
 
   void _onSubmit(String answer) {
     // _timer?.cancel();
+    _onAnimationFinish();
+    setState(() {
+      _selectedTexture = null;
+      isPlaying = false;
+    });
     widget.onResponse("${widget.currentTdtPair.answer.name} & ${widget.currentTdtPair.distractor.name}", answer);
   }
 
@@ -81,11 +99,71 @@ class _TdtPairsTesterState extends State<TdtPairsTester> {
   //   });
   // }
 
+  void _onAnimationStart(ImageTexture newSelectedTexture, int desiredSize) async {
+    _onAnimationFinish();
+    // sl<ActuatorsBloc>().add(InitActuatorsEvent(ActuatorsInitData(
+    //   imgSrc: _selectedTexture!.texture,
+    //   orientation: ActuatorsOrientation.landscape,
+    //   numOfFingers: ActuatorsNumOfFingers.five,
+    //   imagesHeight: desiredSize,
+    //   imagesWidth: desiredSize,
+    // )));
+    await sl<ActuatorsRepository>().initializeActuators(ActuatorsInitData(
+      imgSrc: newSelectedTexture.texture,
+      orientation: ActuatorsOrientation.landscape,
+      numOfFingers: ActuatorsNumOfFingers.five,
+      imagesHeight: desiredSize,
+      imagesWidth: desiredSize,
+    ));
+    setState(() {
+      _selectedTexture = newSelectedTexture;
+    });
+    isPlaying = true;
+    animationController.forward();
+  }
+
+  void _onAnimationFinish() {
+    animationController.reset();
+    // if (currentImageTextureInd < TestingDataProvider.imageTextures.length - 1) {
+    //   setState(() {
+    //     currentImageTextureInd++;
+    //     currentImageTexture = TestingDataProvider.imageTextures[currentImageTextureInd];
+    //     isPlaying = true;
+    //   });
+    //   sl<ActuatorsBloc>().add(LoadImageEvent(ActuatorsImageData(src: currentImageTexture.texture, preload: false)));
+    //   animationController.forward();
+    // }
+  }
+
+  void _renderActuators(double imageSize) {
+    final Offset animatedPosition = AniPatternProvider.verticalPattern(imageSize, animationController.value);
+    sl<ActuatorsBloc>().add(UpdateActuatorsEvent(animatedPosition));
+
+    setState(() {});
+  }
+
   @override
   void initState() {
     super.initState();
     // _sendPattern();
     // _sendPatternRepeatedly();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    );
+
+    animationController.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          isPlaying = false;
+        });
+
+        _onAnimationFinish();
+      }
+    });
+    animationController.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -106,21 +184,98 @@ class _TdtPairsTesterState extends State<TdtPairsTester> {
 
   @override
   Widget build(BuildContext context) {
+    int desiredSize = MediaQuery.of(context).size.width.toInt();
+
+    if (isPlaying) {
+      _renderActuators(desiredSize.toDouble());
+    }
+
     return Column(
       children: [
         const SizedBox(height: 32),
         TestLabel(label: "Item ${widget.currentItemNo} of ${widget.totalItemNo}"),
         const SizedBox(height: 16),
-        const Expanded(
-          flex: 2,
-          child: Center(
-            child: Text(
-              "Which item has a different texture?",
-              style: TextStyle(
-                fontFamily: 'Sailec Medium',
-                fontSize: 20,
-                color: Colors.white,
+        // const Expanded(
+        //   flex: 2,
+        //   child:
+        // ),
+        _selectedTexture == null
+            ? const SizedBox()
+            : BlocProvider(
+                create: (_) => sl<ActuatorsBloc>()
+                  ..add(InitActuatorsEvent(ActuatorsInitData(
+                    imgSrc: _selectedTexture!.texture,
+                    orientation: ActuatorsOrientation.landscape,
+                    numOfFingers: ActuatorsNumOfFingers.five,
+                    imagesHeight: desiredSize,
+                    imagesWidth: desiredSize,
+                  ))),
+                child: SizedBox(
+                  height: desiredSize.toDouble(),
+                  child: _buildBody(_selectedTexture!, desiredSize),
+                ),
               ),
+        Wrap(
+          alignment: WrapAlignment.center,
+          spacing: 8.0,
+          runSpacing: 8.0,
+          children: [
+            ...widget.optionsList.asMap().entries.map(
+              (option) {
+                final index = option.key; // Get the index
+                final tdtOption = option.value; // Get the value (item)
+
+                return ElevatedButton(
+                  onPressed: () => _onAnimationStart(tdtOption, desiredSize),
+                  style: ButtonStyle(
+                    foregroundColor: WidgetStateProperty.all<Color>(
+                      Colors.white,
+                    ),
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                      const Color(0xff128BED),
+                    ),
+                    elevation: WidgetStateProperty.all<double>(0),
+                    shadowColor: WidgetStateProperty.all<Color>(Colors.transparent),
+                    overlayColor: WidgetStateProperty.all<Color>(Colors.transparent),
+                    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: Text((index + 1).toString()),
+                );
+              },
+            ).toList(),
+            ElevatedButton(
+              onPressed: () => _onAnimationFinish(),
+              style: ButtonStyle(
+                foregroundColor: WidgetStateProperty.all<Color>(
+                  Colors.white,
+                ),
+                backgroundColor: WidgetStateProperty.all<Color>(
+                  Colors.red, // Example color for the extra button
+                ),
+                elevation: WidgetStateProperty.all<double>(0),
+                shadowColor: WidgetStateProperty.all<Color>(Colors.transparent),
+                overlayColor: WidgetStateProperty.all<Color>(Colors.transparent),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              child: const Text("Stop"), // Text for the extra button
+            ),
+          ],
+        ),
+        const Center(
+          child: Text(
+            "Which item has a different texture?",
+            style: TextStyle(
+              fontFamily: 'Sailec Medium',
+              fontSize: 20,
+              color: Colors.white,
             ),
           ),
         ),
@@ -160,6 +315,41 @@ class _TdtPairsTesterState extends State<TdtPairsTester> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildBody(ImageTexture currentTexture, int desiredSize) {
+    return BlocBuilder<ActuatorsBloc, ActuatorsState>(
+      builder: (context, state) {
+        if (state is ActuatorsLoading) {
+          return const Center(child: CupertinoActivityIndicator());
+        } else if (state is ActuatorsDone) {
+          return Center(
+            child: Column(
+              children: <Widget>[
+                const Spacer(),
+                Stack(
+                  children: [
+                    Container(
+                      width: desiredSize.toDouble(),
+                      height: desiredSize.toDouble(),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(currentTexture.image),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    ...sl<ActuatorsController>().buildActuators(),
+                  ],
+                ),
+                const Spacer(),
+              ],
+            ),
+          );
+        }
+        return Container();
+      },
     );
   }
 }
